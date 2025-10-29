8a4bc7c91e9fe5bd5f35c2cf56db16244d7cc827
Marco Bacci <marcobacci94@gmail.com>
2025-10-29 17:35:00 +0100

    Fix: snack 6 let count position

--- script.js
+++ script.js
@@ -61,8 +61,8 @@
 // Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
 
 // function creaContatoreAutomatico(tempo) {
+//   let count = 0;
 //   return function () {
-//     let count = 0;
 //     const interval = setInterval(() => {
 //       console.log(count++);
 //       if (count === 10) {
