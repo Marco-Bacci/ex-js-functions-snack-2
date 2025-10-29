75e6672e9bf934492db30234e707b5ea6bb88ef1
Marco Bacci <marcobacci94@gmail.com>
2025-10-29 13:36:20 +0100

    Feat: snack 3 completed

--- script.js
+++ script.js
@@ -12,12 +12,20 @@
 // console.log(somma(3, 6));
 
 // Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
-const somma = (a, b) => a + b
-console.log(somma(3,5))
-
+const somma = (a, b) => a + b;
+console.log(somma(3, 5));
 
 // 2 Crea una arrow function che calcola il quadrato di un numero.
 // Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga
 
-const quadrato = (a) => a*a
-console.log(quadrato(5))
+const quadrato = (a) => a * a;
+console.log(quadrato(5));
+
+// 3) Crea una funzione eseguiOperazione
+// Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
+const moltiplica = (a, b) => a * b;
+function eseguiOperazione(a, b, operazione) {
+  return operazione(a, b);
+}
+console.log(eseguiOperazione(2, 5, moltiplica));
